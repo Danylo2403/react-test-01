9d183d8d0bdd85e2d935b9f72f40cd73e8931b6d
Danylo2403 <daniilkon24@gmail.com>
2023-11-18 01:52:07 +0200

    test commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import { Profile } from './Profile/Profile';
+
+const jacob = {
+  id: 1,
+  name: 'Jacob Mercer',
+  email: 'j.mercer@mail.com',
+  age: 32,
+  avatar: 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
+};
+
+const olendau = {
+  id: 3,
+  name: 'Olendau Jackson',
+  email: 'o.jaclson@mail.com',
+  age: 39,
+  avatar: 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
+}
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div>
+      <Profile
+        username={jacob.name}
+        imgUrl={jacob.avatar}
+        age={jacob.age}
+        email={jacob.email}
+      />
+      
+      <Profile
+        username={olendau.name}
+        imgUrl={olendau.avatar}
+        age={olendau.age}
+        email={olendau.email}
+      />
+      </div>
   );
 };
