d83d70dc37fd57b81d54548bcd8374691eb6ec60
Danylo2403 <daniilkon24@gmail.com>
2023-11-20 01:28:29 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,13 @@
-import { Profile } from './Profile/Profile';
-
-const jacob = {
-  id: 1,
-  name: 'Jacob Mercer',
-  email: 'j.mercer@mail.com',
-  age: 32,
-  avatar: 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
-};
-
-const olendau = {
-  id: 3,
-  name: 'Olendau Jackson',
-  email: 'o.jaclson@mail.com',
-  age: 39,
-  avatar: 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
-}
-
+import { FaBeer } from 'react-icons/fa';
+import { PilotList } from './PilotList';
+import pilots from '../pilots.json';
 
 export const App = () => {
   return (
     <div>
-      <Profile
-        username={jacob.name}
-        imgUrl={jacob.avatar}
-        age={jacob.age}
-        email={jacob.email}
-      />
+      <h1>Top rated pilots <FaBeer size={40}/></h1>
       
-      <Profile
-        username={olendau.name}
-        imgUrl={olendau.avatar}
-        age={olendau.age}
-        email={olendau.email}
-      />
+      <PilotList pilots={ pilots } />
       </div>
   );
 };
